0b02eeff44f4acf0c5dc3f92a02697dc9f4ac540
mahakarzoun <62464398+mahakarzoun@users.noreply.github.com>
2020-08-08 07:53:32 +0200

    Update flight-booking.js

--- javascript/javascript1/week2/flight-booking.js
+++ javascript/javascript1/week2/flight-booking.js
@@ -1,9 +1,14 @@
-function getfullname(firstname, lastname, useformal){
-  fullName = firstname + '' + lastname;
-  if (useformal) {
-    fullName = 'MR '+ fullName;
-    return fullName;
-  }
+let fullName1;
+let fullName2;
+let formalFullName = true;
+function getFullName (firstName, surName, formalFullName) {
+fullName1 = `lord ${firstName} ${surName}`;
+fullName2 = `${firstName} ${surName}`;
+if (formalFullName) {
+  return fullName1;
+} else {
+  return fullName2;
 }
-  console.log(getfullname('Benjamin ', 'hughes ', true));
-  console.log(getfullname('Benjamin ', 'hughes ', false));
+}
+getFullName('Benjamin', 'Hughes', true);
+getFullName('Benjamin', 'Hughes', false);
