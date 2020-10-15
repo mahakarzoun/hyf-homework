86f2ae683a9bcff72531739e26dc4826368cd01e
maha.karzoun@gmail.com <maha.karzoun@gmail.com>
2020-10-15 15:21:40 +0200

    implemented feedback

--- javascript/javascript2/week1/animal-spirit/main.js
+++ javascript/javascript2/week1/animal-spirit/main.js
@@ -1,33 +1,3 @@
-// functions 
-function getAnimalNumber() {
-  let max = spiritualtraits.length;
-  return Math.floor(Math.random() * (max  + 1)) ;
-}
-
-function getAnilmalTraitsIndex() {
-  let max = 3;
-  return Math.floor(Math.random() * (max + 1)) ;
-}
-
-function getYourSpiritualAnimal() {
-  let animalIndex = getAnimalNumber();
-  let TraitsIndex = getAnilmalTraitsIndex();
-  let spices = spiritualtraits[animalIndex].traits[TraitsIndex];
-  let animalName = spiritualtraits[animalIndex].name;
-  return `${spices} ${animalName}`
-}
-
-
-
-function LogOut (e) {
-  debugger
-  const input = document.querySelector("input[name='username']");
-  const name = input.value;
-  const Animal = getYourSpiritualAnimal();
-  let output = document.createElement('p')
-  output.innerText = `${name} the ${Animal}` 
-  bodyPage.appendChild(output)
-}
 // variables 
 const spiritualtraits = [
   { name: 'fox', traits: ['secretive', 'charming', 'Sneaky', 'curious'] },
@@ -39,7 +9,38 @@ const spiritualtraits = [
   { name: 'dove', traits: ['Trustworthy', 'Easygoing', 'Dependent', 'Predictable'] }
 ]
 
+
+// functions 
+function getAnimalNumber() {
+  const max = spiritualtraits.length;
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function getAnilmalTraitsIndex() {
+  const max = 3;
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function getYourSpiritualAnimal() {
+  const animalIndex = getAnimalNumber();
+  const TraitsIndex = getAnilmalTraitsIndex();
+  const spices = spiritualtraits[animalIndex].traits[TraitsIndex];
+  const animalName = spiritualtraits[animalIndex].name;
+  return `${spices} ${animalName}`
+}
+
+function LogOut(e) {
+  debugger
+  const input = document.querySelector("input[name='username']");
+  const name = input.value;
+  const Animal = getYourSpiritualAnimal();
+  let output = document.createElement('p')
+  output.innerText = `${name} the ${Animal}`
+  bodyPage.appendChild(output)
+}
+
+
 //setting up the event 
-let bodyPage = document.querySelector("body")
-let btn = document.querySelector("#genbutton");
- btn.addEventListener("click",LogOut);
+const bodyPage = document.querySelector("body")
+const btn = document.querySelector("#genbutton");
+btn.addEventListener("click", LogOut);
