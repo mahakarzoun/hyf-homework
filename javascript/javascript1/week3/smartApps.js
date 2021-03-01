9f0cb4548a225deabedbe0a6703aca7ba872035d
maha.karzoun@gmail.com <maha.karzoun@gmail.com>
2021-03-01 23:37:14 +0100

    add mostTime function

--- javascript/javascript1/week3/smartApps.js
+++ javascript/javascript1/week3/smartApps.js
@@ -4,76 +4,82 @@ let day = date.getDate();
 let months = date.getMonth() + 1;
 let year = date.getFullYear();
 
-function getSum (arr) {
+function getSum(arr) {
   let counter = arr.reduce((a, b) => {
-  return a + b 
-})
-return counter 
+    return a + b
+  })
+  return counter
 }
 
-function getTime (arr){
-  let time = getSum(arr)
+function getTime(time) {
   let hours = Math.floor(time / 60)
-  let minuts = Math.round((time % 60) - hours) //
-  const timeArr = [time,hours,minuts]
+  let minuts = time % 60
+  const timeArr = [time, hours, minuts]
   return timeArr
 }
 
 
-function addActivity(activity,duration) {
-  let data = `${day}-${months}-${year}`
-  const obj = { "data": `${data}`, "activity": `${activity}`, "duration": duration }
+function addActivity(activity, duration) {
+  if (typeof activity != 'string' || typeof duration != 'number')
+    throw new Error('Illegal Argument Exception');
+
+  let date = `${day}-${months}-${year}`
+  const obj = { 'date': `${date}`, 'activity': `${activity}`, 'duration': duration }
   activities.push(obj)
   return obj
 }
 
 function showStatus(date) {
+  if (activities.length === 0) {
+    return 'Add some activities before calling showStatus';
+  }
   // check if date is there 
-  const filteredArr = activities.filter(obj => obj.data === date)
-  const mapped = filteredArr.map(obj => obj.duration) // move to 38 ??
+  const filteredArr = activities.filter(obj => obj.date === date)
   if (filteredArr.length === 0) {
-    console.log("no activities at this date")
+    console.log('no activities at this date')
+    return;
   }
-  else {
-    // number of activity 
-    const numOfActivities = filteredArr.length;
-    if (numOfActivities > 0) { // but it shouldnt be in the first place sould i still have it 
-      /*for (let i = 0; i < numOfActivities; i++) {
-        sum = filteredArr[i].duration
-        sum += sum*/
-      let sumOfTime = getSum(mapped);
-      let time = getTime(mapped);
+  const mapped = filteredArr.map(obj => obj.duration)
+  // number of activity 
+  const numOfActivities = filteredArr.length;
+  if (numOfActivities > 0) {
+    let sumOfTime = getSum(mapped);
+    let time = getTime(sumOfTime);
 
-      if (sumOfTime > 240) {
-        console.log("You have reached your limit, no more smartphoning for you!")
-      }
-      else if (60 < sumOfTime && sumOfTime <= 240) {
-        console.log(`You have added ${numOfActivities} activities.They amount to ${time[1]} h and ${time[2]} min of usage`)
-      } else {
-        console.log(`You have added ${numOfActivities} activities.They amount to ${time[0]} min of usage`)
-      }
+    if (sumOfTime > 240) {
+      return 'You have reached your limit, no more smartphoning for you!'
     }
+    if (60 < sumOfTime && sumOfTime <= 240) {
+      return `You have added ${numOfActivities} activities.They amount to ${time[1]} h and ${time[2]} min of usage`
+    }
+    return `You have added ${numOfActivities} activities.They amount to ${time[0]} min of usage`
   }
 }
 
-function mostTime () {
-// declare an obj with act as key and 8add it as a value to keys  map()
-let arrOfActivities = activities.filter(function (obj) {
-   if (obj.activity.toLowerCase() !== obj.activity)
-return obj})/*.map(function (obj) {
-  obj = {`${obj.activity}: ${getTime(obj.activity)}`}
-})*/
-console.log(arrOfActivities)
 
-// use the same func to show status to sum thetime .
-// return the key with the heighst value 
+function mostTime(array) {
+  const combinedActivities = array.reduce((result, act) => {
+    if (act.hasOwnProperty('activity') && act.hasOwnProperty('duration')) {
+      const activity = result.find(a => a.activity === act.activity.toLowerCase())
+      if (activity)
+        activity.duration += act.duration;
+      else
+        result.push({ activity: act.activity.toLowerCase(), duration: act.duration })
+    }
+    return result;
+  }, []);
+  if (combinedActivities.length === 0)
+    return 'no valid activity was found'
+  //mda =maxDurationActivity
+  const mda = combinedActivities.reduce((max, act) =>
+    max.duration > act.duration ? max : act
+    , combinedActivities[0])
+  return `You has spent the most time on ${mda.activity} with ${mda.duration} m`;
 }
-
-// or map to loercae then gettime(activity)
-
-addActivity('youtube', 30);
+console.log(showStatus('1-3-2021'))
 addActivity('Youtube', 40);
 addActivity('Facebook', 20);
-console.log(showStatus('21-2-2021'));
- mostTime () 
-
+addActivity('Facebook', 20);
+console.log(showStatus('1-3-2021'))
+console.log(activities)
+console.log(mostTime(activities))
