9f1dc326f827405854d048f3ea8937f8e9196eb4
maha.karzoun@gmail.com <maha.karzoun@gmail.com>
2021-03-15 22:30:25 +0100

    modify homework

--- javascript/javascript2/week2/hyf-bay/main.js
+++ javascript/javascript2/week2/hyf-bay/main.js
@@ -1,4 +1,27 @@
 console.log("Script loaded");
-
 const products = getAvailableProducts();
-console.log(products);
+const ul = document.querySelector("ul");
+
+const input = document.querySelector('input')
+input.addEventListener('input', filterProducts)
+
+function filterProducts(event) {
+    const searchText = input.value
+    const filteredProducts =
+        searchText.length === 0 ?
+            [] : products
+                .filter(element => element.name.includes(searchText))
+    renderProducts(filteredProducts);
+}
+
+function renderProducts(products) {
+    // your code here
+    ul.innerHTML = "";
+    products.forEach(product => {
+        const li = document.createElement("li");
+        li.innerHTML = `<h2>${product.name}</h2><span>price: ${product.price}</span><br><span>Rating: ${product.rating}</span>`;
+        ul.appendChild(li);
+    });
+}
+
+
