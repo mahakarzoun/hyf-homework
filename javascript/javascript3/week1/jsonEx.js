171176706366158f7f9353fe426a4d7bfb57d8d8
maha.karzoun@gmail.com <maha.karzoun@gmail.com>
2021-03-25 15:15:29 +0100

    modify some in-class exercises

--- javascript/javascript3/week1/jsonEx.js
+++ javascript/javascript3/week1/jsonEx.js
@@ -1,4 +1,5 @@
-/*let userData  = [{
+body = document.querySelector('body')
+let userData = [{
     name: "maha",
     age: 24,
     city: "aarhus"
@@ -13,9 +14,19 @@ console.log(typeof jsonFile);
 document.getElementById("par").innerHTML = jsonFile;
 let undojson = JSON.parse(jsonFile)
 console.log(undojson)
-document.getElementById("par2").innerHTML = undojson[0].name;*/
+document.getElementById("par2").innerHTML = undojson[0].name;
 
 
 fetch('https://reqres.in/api/users')
-  .then(res =>  res.json())
-  .then(data => console.log(data))
+    .then(res => res.json())
+    .then(data => {
+        let userInfo = data.data;
+        console.log(userInfo)
+        userInfo.forEach(user => {
+            let p = document.createElement('p')
+            p.innerText = `${user.first_name}${user.last_name}`
+            body.appendChild(p)
+        })
+
+    })
+
