b3a2fb87bdc299161f13184752680b16903f35c2
maha.karzoun@gmail.com <maha.karzoun@gmail.com>
2021-04-14 23:45:31 +0200

    Remove the commented-out code

--- javascript/javascript3/week1/in_class_Exercises/main.js
+++ javascript/javascript3/week1/in_class_Exercises/main.js
@@ -11,42 +11,4 @@ const data = fetch('https://dog.ceo/api/breeds/image/random')
       h2.innerHTML = catFact.text
       body.appendChild(h2)
     })
-  }
-  
-
-/*const body = document.querySelector('body')
-const data = fetch('http://api.open-notify.org/astros.json').then(
-  (response) => response.json().then((data) => data.people.forEach(person =>  {
-    const h2 = document.createElement('h2')
-        h2.innerHTML = person.name
-        body.appendChild(h2)  
-  })
- )
-)
-
-
-const promise = fetch('http://api.open-notify.org/astros.json')
-  .then(
-    (response) => response.json()
-      .then((data) => (renderName)
-      )
-  )
-
-function renderName () {}
-      {
-        const h1 = document.createElement('h1')
-        h1.innerHTML = `there are ${data.number} astronaut in space, they are`
-        body.appendChild(h1)
-
-        data.people.forEach((person) => {
-          const h2 = document.createElement('h2')
-          h2.innerHTML = person.name
-          body.appendChild(h2)
-        })
-      }
-
-
-/*
-fetch.then (fetch the response) and then call json then (function (data))
-we can call , then in the end
-*/
+  }  
