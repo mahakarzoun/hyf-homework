b3953257cc622df83f5afc107b8d8f1c3cab4235
maha.karzoun@gmail.com <maha.karzoun@gmail.com>
2021-04-15 00:31:27 +0200

    implement feedback

--- javascript/javascript2/week3/Exercises/main.js
+++ javascript/javascript2/week3/Exercises/main.js
@@ -1,26 +1,22 @@
-// variables 
+// variables //
 // counter 
 const body = document.querySelector('body');
 const counterBtn = document.querySelector('button')
 let count = -1
 // delayBtn
 const delayBtn = document.getElementById('delay')
-let message;
 const delayMessage = document.getElementById('delayMessage');
+
 //mouseposition
-let coords;
 let mousePositionList = [];
 //const averageCalculater= (preValue, currValue) => ((preValue + currValue) / mousePositionList.length)
 
-
-
-
 counterBtn.addEventListener('click', () => {
     count += 1
-    counterBtn.innerHTML = `counter : ${count}`;
+    counterBtn.innerHTML = `counter: ${count}`;
 })
 
-function logMessage() {
+function logMessage(message) {
     message = 'log in 3 seconds';
     delayMessage.innerText = message
     // message as alocal variable in thre function logmessage
@@ -34,9 +30,6 @@ function logMessage() {
 delayBtn.addEventListener('click', logMessage
 )
 
-/*() => {
-    logMessage
-    delayBtn.innerText=message  it doesnt work } */
 
 //page onload 
 const fullyLoaded = () => {
@@ -44,32 +37,31 @@ const fullyLoaded = () => {
     onload.innerText = "DOM fully loaded and parsed";
 }
 window.addEventListener('load', fullyLoaded)
-//window.onload = fullyLoad
 
-// mous position 
+
+// mouse position 
 // how to know the position of the mouse 
 // creat a callback function settimeout 
 // mouse event in the eventlistiner 
-
 function Mouseposition(event) {
     const x = event.clientX;
     const y = event.clientY;
-    coords = { 'x': x, 'y':y }
-    const coordsForPointer = "X coords: " + x + ", Y coords: " + y;
+    const coords = { 'x': x, 'y': y }
+    const coordsForPointer = `X coords:${x} , Y coords:  ${y}`;
     document.getElementById("showCoords").innerText = coordsForPointer;
     mousePositionList.push(coords)
 }
-window.addEventListener('mouseover', 
+window.addEventListener('mouseover',
     Mouseposition
 )
 
-setTimeout(()=> {
-    window.removeEventListener('mouseover',Mouseposition);
-    const x =  mousePositionList.map(element => element.x)
-               .reduce((x1,x2)=>x1+x2)
-    const y =  mousePositionList.map(element => element.y)
-               .reduce((y1,y2)=>y1+y2)
-    const averageX = Math.round(x/mousePositionList.length)
-    const averageY = Math.round(y/mousePositionList.length)
-    console.log(`average x and y position of a user after 30 seconds : (${averageX},${averageY})`)  
+setTimeout(() => {
+    window.removeEventListener('mouseover', Mouseposition);
+    const x = mousePositionList.map(element => element.x)
+        .reduce((x1, x2) => x1 + x2)
+    const y = mousePositionList.map(element => element.y)
+        .reduce((y1, y2) => y1 + y2)
+    const averageX = Math.round(x / mousePositionList.length)
+    const averageY = Math.round(y / mousePositionList.length)
+    console.log(`average x and y position of a user after 30 seconds : (${averageX},${averageY})`)
 }, 30000)
