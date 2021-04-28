5498b0c1441936ee4416620775a90b15cdd61d0c
maha.karzoun@gmail.com <maha.karzoun@gmail.com>
2021-04-28 05:16:09 +0200

    add some changes n the queris

--- databases/week2/part3.sql
+++ databases/week2/part3.sql
@@ -6,7 +6,7 @@ from task
  Inner join user_task ON  user_task.task_id = task.id 
  inner join user ON  user.id = user_task.user_id 
  WHERE user.email LIKE '%@spotify.com';
-
+ 
  -- Get all the tasks for 'Donald Duck' with status 'Not started'
 SELECT  task.id AS taskID , task.title AS not_started_tasks 
 From task
@@ -18,11 +18,17 @@ INNER join status ON task.status_id= Status.id
 WHERE user.name = 'Donald Duck' AND status.name = 'not started';
 
 -- Get all the tasks for 'Maryrose Meadows' that were created in september (hint: month(created)=month_number)
-SELECT  task.id AS taskID , task.title AS septemberTask 
+SELECT  task.id AS taskID , task.title AS septemberTasks, task.created
 From task
-iNNER join user_task ON  
+join user_task ON  
 task.id = user_task.task_id -- why the deplicate values 
-INNER join user ON
+join user ON
 user_task.user_id = user.id 
-INNER join status ON task.status_id= Status.id 
-WHERE user.name = 'Maryrose Meadows' AND task.created(month) = 'september';
+join status ON task.status_id= Status.id 
+WHERE user.name = 'Maryrose Meadows' AND monthname(task.created) = 'september';
+
+/*Find how many tasks where created in each month, e.g. how many tasks were created in october,
+ how many tasks were created in november, etc. (hint: use group by)*/
+ SELECT count(task.id), monthName(task.created)
+ from task
+GROUP BY monthName(task.created);
