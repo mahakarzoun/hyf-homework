328898b4b04f2d78f76f086f5ce8aeab250b0cfc
name <maha.karzoun@gmail.com>
2021-05-11 19:59:49 +0200

    implement warmup exercise

--- node.js/week1/homework/warmup.js
+++ node.js/week1/homework/warmup.js
@@ -1,1 +1,21 @@
 console.log("inside warmup file");
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius
+    }
+
+    get Diameter() {
+        return this.radius * 2
+    }
+    get Circumference() {
+        return Math.PI * this.radius * 2
+    }
+
+    get Area() {
+        return Math.pow(this.radius, 2) * Math.PI
+    }
+}
+
+const circle = new Circle(2);
+console.log(circle.Diameter)
