f350eb5ed9d4b627199196ae3d12018324ff97ea
name <maha.karzoun@gmail.com>
2021-05-11 19:37:40 +0200

    implement feedback

--- node.js/week1/exercise-template/src/backend/index.js
+++ node.js/week1/exercise-template/src/backend/index.js
@@ -3,9 +3,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const components = require("./components")
 
 app.get("/", (request, response) => {
   response.send(`
+  ${components.head('home')}
     <body>
         <h1>My portfolio</h1>
     </body>
@@ -14,6 +16,7 @@ app.get("/", (request, response) => {
 
 app.get("/contact", (request, response) => {
   response.send(`
+  ${components.head('Contact')}
     <body>
         <h1>Contact</h1>
         <p>This is a contact page</p>
@@ -21,7 +24,48 @@ app.get("/contact", (request, response) => {
   `);
 });
 
-app.get('/test-report', function(requset, response) {
+app.get("/education", (request, response) => {
+  response.send(`
+  ${components.head('Education')}
+    <body>
+        <h1>Education</h1>
+        <p>This is a Education page</p>
+    </body>
+  `);
+});
+
+app.get("/skills", (request, response) => {
+  response.send(`
+  ${components.head('Skills')}
+    <body>
+        <h1>Skills</h1>
+        <p>This is a Skills page</p>
+    </body>
+  `);
+});
+
+app.get("/projects", (request, response) => {
+  response.send(`
+  <html>
+      ${components.head('Projects')}
+      <body>
+        <h1>Projects</h1>
+        <ul>
+          <li>
+            <p>codeUrl:
+            <a href="https://github.com/mahakarzoun/hyf-homework/blob/master/javascript/javascript2/week3/theGame.js.js">the press game</a>
+            </p>
+            <p>previewUrl:</p>
+            <img alt="homework folder" src="https://github.com/HackYourFuture-CPH/JavaScript/raw/master/javascript2/week3/homework/fastest-clicker.gif" width="400" style="max-width:100%;">
+          <li>
+        </ul>
+      </body>
+    </html>
+  `);
+});
+
+
+app.get('/test-report', function (requset, response) {
   response.sendFile(path.join(__dirname + '/test-report.html'));
 });
 
