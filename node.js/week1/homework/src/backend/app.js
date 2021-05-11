f4e744c11469ccd0239bd55704cfc5d0778c8a38
name <maha.karzoun@gmail.com>
2021-05-11 20:12:20 +0200

    implement feedback

--- node.js/week1/homework/src/backend/app.js
+++ node.js/week1/homework/src/backend/app.js
@@ -8,72 +8,70 @@ const reviews = require("./data/reviews");
 const reservations = require("./data/reservations");
 
 //render the data in meals route 
-const  meals_reviewsJson = meals.map(meal => {
+const meals_reviewsJson = meals.map(meal => {
   const mealsReview = reviews.filter(review => review.mealId === meal.id)
   meal.reviews = mealsReview
   return meal
-}) 
+})
 
 //render the data in cheap_meals route
-const  cheapMeal = meals_reviewsJson.filter(meal => meal.price < 80)
+const cheapMeal = meals_reviewsJson.filter(meal => meal.price < 80)
 
 //render the data in large_meals route 
-const  largeMeal = meals_reviewsJson.filter(meal => meal.maxNumberOfGuests > 4 )
+const largeMeal = meals_reviewsJson.filter(meal => meal.maxNumberOfGuests > 4)
 
 //render the data of a random meal in meal route 
-function  randomNumber (array) { // what can  name the obj ?
-  return Math.floor(Math.random() * array.length) + 0;
+function randomNumber(array) {
+  return Math.floor(Math.random() * array.length);
 }
-function randomMeal (){
-   return meals_reviewsJson[randomNumber(meals)]
+function randomMeal() {
+  return meals_reviewsJson[randomNumber(meals)]
 }
 
 //render the data of a random meal in meal route
-function randomReservation (){
-    return reservations[randomNumber(reservations)]
+function randomReservation() {
+  return reservations[randomNumber(reservations)]
 }
 
 
 // this is where you will be adding your routes
 app.get("/", async (request, response) => {
-  response.setHeader('content-type','text/plain')
-  response.send("mealsharing app");
+  response.setHeader('content-type', 'text/plain')
+  response.send("Meal Sharing Web App");
 });
 
 app.get("/meals", async (request, response) => {
- response.setHeader('content-type','application/json')
-  //response.send(meals_reviewsJson)
+  response.setHeader('content-type', 'application/json')
   response.send(meals_reviewsJson)
 });
 
-app.get("/cheap_meals", async (request, response) => {
-  response.setHeader('content-type','application/json')
-   //response.send(meals_reviewsJson)
-   response.send(cheapMeal)
+app.get("/cheap-meals", async (request, response) => {
+  response.setHeader('content-type', 'application/json')
+  response.send(cheapMeal)
 })
 
 //render the data in large_meals route
-app.get("/large_meals", async (request, response) => {
-  response.setHeader('content-type','application/json')
-   response.send(largeMeal)                                                              
+app.get("/large-meals", async (request, response) => {
+  response.setHeader('content-type', 'application/json')
+  response.send(largeMeal)
 })
 
 //render the data in meal route
 app.get("/meal", async (request, response) => {
-  response.setHeader('content-type','application/json')
+  response.setHeader('content-type', 'application/json')
   response.send(randomMeal())
 })
 
 //render the data in reservations route
 app.get("/reservations", async (request, response) => {
-  response.setHeader('content-type','application/json')
-   response.send(reservations)
+  response.setHeader('content-type', 'application/json')
+  response.send(reservations)
 })
 
 //render the random reservation in reservation route
 app.get("/reservation", async (request, response) => {
-  response.setHeader('content-type','application/json')
-   response.send(randomReservation())
+  response.setHeader('content-type', 'application/json')
+  response.send(randomReservation())
 })
 
 module.exports = app;
